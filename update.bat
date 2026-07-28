6ea1e8ad99003a3e9fd7c7f6a958d324b1a28495
scheepsapp <scheepsapp@gmail.com>
2026-07-28 20:00:21 +0200

    1.1.4

--- update.bat
+++ update.bat
@@ -8,7 +8,7 @@ echo ============================================
 :: 1. Controleer op Admin rechten
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo [FOUT] Rechtermuisknop -> Als administrator uitvoeren.
+    echo [FOUT] Rechtermuisknop -^> Als administrator uitvoeren.
     pause
     exit /b
 )
@@ -43,12 +43,18 @@ if exist "%appDir%\waterstanden.html" attrib -h -s -r "%appDir%\waterstanden.htm
 move /y "%TEMP%\waterstanden.html" "%appDir%\waterstanden.html" >nul
 attrib +h +s +r "%appDir%\waterstanden.html"
 
-:: Nieuw bestand: reefer.html
 echo Bezig met reefer.html...
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%baseUrl%/reefer.html' -OutFile '%TEMP%\reefer.html'"
 if exist "%appDir%\reefer.html" attrib -h -s -r "%appDir%\reefer.html"
 move /y "%TEMP%\reefer.html" "%appDir%\reefer.html" >nul
 attrib +h +s +r "%appDir%\reefer.html"
+
+:: Nieuw bestand: stabiliteit.html
+echo Bezig met stabiliteit.html...
+powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%baseUrl%/stabiliteit.html' -OutFile '%TEMP%\stabiliteit.html'"
+if exist "%appDir%\stabiliteit.html" attrib -h -s -r "%appDir%\stabiliteit.html"
+move /y "%TEMP%\stabiliteit.html" "%appDir%\stabiliteit.html" >nul
+attrib +h +s +r "%appDir%\stabiliteit.html"
 
 echo.
 echo ============================================
